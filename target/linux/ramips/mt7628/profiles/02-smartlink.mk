56b5279b4b236c9b304220a32e5109ea961a4cde
Wuguanfa <wuguanfa@wuguanfadeMBP.lan>
2017-04-23 23:58:31 +0800

    Start SmartLink project
    Add SmartLink MT7628 iot board in openwrt
    Add mt7628 SmartLink SDK wifi
    Add wifi ui for luci-base

---
+++ target/linux/ramips/mt7628/profiles/02-smartlink.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2015 seatry.com
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/SmartLink
+ NAME:=ivotten router
+ PACKAGES:=\
+	kmod-fs-exfat kmod-fs-ext4 kmod-fs-hfs kmod-fs-hfsplus kmod-fs-nfs kmod-fs-nfs-common kmod-fs-ntfs \
+	kmod-fs-vfat \
+	kmod-nls-cp437 kmod-nls-iso8859-1 kmod-nls-utf8 \
+	ip ipset libpolarssl iptables-mod-tproxy \
+	kmod-nfnetlink kmod-ipt-ipset kmod-ipt-tproxy libmnl libpthread
+
+
+endef
+
+define Profile/SmartLink/Description
+ Package set for SmartLink
+endef
+
+$(eval $(call Profile,SmartLink))
+
